ac693bd26276589ff6e3369e40302fef746c13a8
hetpatel07 <hets7878401023@gmail.com>
2019-02-25 17:37:01 +0530

    lunatic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+function popup() {
+    document.getElementById('display').style.display = "block";
+}
+
+function popdown() {
+    document.getElementById('display').style.display = "none";
+}
